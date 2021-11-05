24094f90d1cc7cbfc063feaedf3dc3537934ba73
maya800800 <m-wakabayashi0274@st.kyoto-art.ac.jp>
2021-11-05 19:19:51 +0900

    a

--- js/main.js
+++ js/main.js
@@ -274,7 +274,14 @@ $(function(){
   function () {
     $(this).removeClass("hover");
   });
- 
-    
+
+  var card = $(".card");
+
+  $(document).on("mousemove",function(e) {  
+    var ax = -($(window).innerWidth()/2- e.pageX)/20;
+    var ay = ($(window).innerHeight()/2- e.pageY)/10;
+    card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
+ 　});
+  
 }
   
